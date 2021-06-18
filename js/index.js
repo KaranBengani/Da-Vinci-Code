aaaa1855053842c8abee68859ceb2a777ecf21a6
KaranBengani <63871616+KaranBengani@users.noreply.github.com>
2021-06-19 02:59:39 +0530

    :lipstick:

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+// 1st set of event listeners
+
+var hi = document.getElementsByClassName("living");
+for (var i = 0; i < hi.length; i++) {
+  hi[i].addEventListener("click", visible1);
+}
+
 // popup code
 
 function visible1() {
@@ -24,9 +31,11 @@ function room2colour() {
   for (var i = 0; i < add.length; i++) {
     add[i].classList.add("blink");
   }
-  var wall1 = document.getElementById("bedleft");
-  wall1.style.cursor = "pointer";
-  wall1.addEventListener("click", visible2);
+  var wall1 = document.getElementsByClassName("bedcur");
+  for (var i = 0; i < wall1.length; i++) {
+    wall1[i].style.cursor = "pointer";
+    wall1[i].addEventListener("click", visible2);
+  }
 }
 function room3colour() {
   var add = document.getElementsByClassName("studyroom");
@@ -36,20 +45,32 @@ function room3colour() {
 }
 
 // hide functions
+
+// hide to hide living room
 function hide() {
   var rem = document.getElementsByClassName("living");
   for (var i = 0; i < rem.length; i++) {
     rem[i].classList.remove("blink");
   }
+  var hl = document.getElementsByClassName("living");
+  for (var i = 0; i < hl.length; i++) {
+    hl[i].removeEventListener("click", visible1);
+    hl[i].style.cursor = "auto";
+  }
   setTimeout(() => {
     room2colour();
   }, 1000);
 }
-
+// hide to hide bed room
 function hide2() {
   var rem = document.getElementsByClassName("bedroom");
   for (var i = 0; i < rem.length; i++) {
     rem[i].classList.remove("blink");
+  }
+  var hl = document.getElementsByClassName("bedroom");
+  for (var i = 0; i < hl.length; i++) {
+    hl[i].removeEventListener("click", visible2);
+    hl[i].style.cursor = "auto";
   }
   setTimeout(() => {
     room3colour();
