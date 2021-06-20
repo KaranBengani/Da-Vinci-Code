28739616a907e544a83974ebe195dbd48a5987d5
KaranBengani <63871616+KaranBengani@users.noreply.github.com>
2021-06-20 15:19:28 +0530

    :lipstick:

--- js/index.js
+++ js/index.js
@@ -107,7 +107,7 @@ function room5colour() {
 // hide functions
 
 // hide to hide living room
-function hide() {
+function hide(livingthis) {
   var rem = document.getElementsByClassName("living");
   for (var i = 0; i < rem.length; i++) {
     rem[i].classList.remove("blink");
@@ -117,11 +117,12 @@ function hide() {
   setTimeout(() => {
     room2colour();
   }, 1000);
-
+  var update1 = livingthis.id;
+  update(update1);
   visible1();
 }
 // hide to hide bed room
-function hide2() {
+function hide2(bedthis) {
   var rem = document.getElementsByClassName("bedroom");
   for (var i = 0; i < rem.length; i++) {
     rem[i].classList.remove("blink");
@@ -131,12 +132,13 @@ function hide2() {
   setTimeout(() => {
     room3colour();
   }, 1000);
-
+  var update2 = bedthis.id;
+  update(update2);
   visible2();
 }
 
 // hide to hide study room
-function hide3() {
+function hide3(studythis) {
   var rem = document.getElementsByClassName("studyroom");
   for (var i = 0; i < rem.length; i++) {
     rem[i].classList.remove("blink");
@@ -146,12 +148,13 @@ function hide3() {
   setTimeout(() => {
     room4colour();
   }, 1000);
-
+  var update3 = studythis.id;
+  update(update3);
   visible3();
 }
 
 // hide to hide store room
-function hide4() {
+function hide4(storethis) {
   var rem = document.getElementsByClassName("storeroom");
   for (var i = 0; i < rem.length; i++) {
     rem[i].classList.remove("blink");
@@ -161,17 +164,52 @@ function hide4() {
   setTimeout(() => {
     room5colour();
   }, 1000);
-
+  var update4 = storethis.id;
+  update(update4);
   visible4();
 }
 
 // hide to hide kitchen room
-function hide5() {
+function hide5(kitchenthis) {
   var rem = document.getElementsByClassName("kitchen");
   for (var i = 0; i < rem.length; i++) {
     rem[i].classList.remove("blink");
     rem[i].removeEventListener("click", visible5);
     rem[i].style.cursor = "auto";
   }
+  var update5 = kitchenthis.id;
+  update(update5);
   visible5();
+}
+
+// Report update functions
+
+// function call after living room
+function update(comparison) {
+  if (comparison == "livingroomfin") {
+    const para1 = document.createElement("p");
+    const node1 = document.createTextNode("update from Living room");
+    para1.appendChild(node1);
+    document.getElementById("reportcontent").appendChild(para1);
+  } else if (comparison == "bedroomfin") {
+    const para2 = document.createElement("p");
+    const node2 = document.createTextNode("update from bed room");
+    para2.appendChild(node2);
+    document.getElementById("reportcontent").appendChild(para2);
+  } else if (comparison == "studyroomfin") {
+    const para3 = document.createElement("p");
+    const node3 = document.createTextNode("update from study room");
+    para3.appendChild(node3);
+    document.getElementById("reportcontent").appendChild(para3);
+  } else if (comparison == "storeroomfin") {
+    const para4 = document.createElement("p");
+    const node4 = document.createTextNode("update from store room");
+    para4.appendChild(node4);
+    document.getElementById("reportcontent").appendChild(para4);
+  } else if (comparison == "kitchenfin") {
+    const para5 = document.createElement("p");
+    const node5 = document.createTextNode("update from Kitchen");
+    para5.appendChild(node5);
+    document.getElementById("reportcontent").appendChild(para5);
+  }
 }
